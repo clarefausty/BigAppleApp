87cebbf467adb1e31ef9cfddc522b1bee101238d
clarefausty <clarefausty@gmail.com>
2023-11-24 23:47:51 +0100

    wip

--- src/components/details/Detail.js
+++ src/components/details/Detail.js
@@ -1,18 +1,16 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Container, Form } from "react-bootstrap";
 
 const Detail = () => {
-
   const [isViolation, setIsViolation] = useState(false);
 
   const handleCheckboxChange = (event) => {
-    setIsViolation(event.target.id === 'yes');
+    setIsViolation(event.target.id === "yes");
   };
   return (
     <div>
       <Container>
-        <Form>
+        <Form class="text-uppercase">
           <div class="mb-3 d-sm-flex mt-5 gap-3">
             <label for="estimate" class="form-label">
               Estimate
@@ -42,31 +40,31 @@ const Detail = () => {
               aria-describedby="emailHelp"
             />
           </div>
-          <div class="mb-3 d-sm-flex mt-5 gap-3 gx-3">
+          <div class="mb-3 d-sm-flex  gap-3 ">
             <label for="" class="form-label">
               Building Address
             </label>
             <input
               type="text"
-              class="form-control"
+              class="form-control mb-2"
               id=""
               placeholder="Street"
             />
             <input
               type="text"
-              class="form-control my-3"
+              class="form-control mb-2"
               id=""
               placeholder="City"
             />
             <input type="text" class="form-control" id="" placeholder="NY" />
             <input
               type="text"
-              class="form-control my-3"
+              class="form-control mb-2"
               id=""
               placeholder="Zipcode"
             />
           </div>
-          <div class="mb-3 d-sm-flex mt-5 gap-3">
+          <div class="mb-3 d-sm-flex  gap-3">
             <label for="estimate" class="form-label">
               Req By
             </label>
@@ -95,35 +93,59 @@ const Detail = () => {
               aria-describedby="emailHelp"
             />
           </div>
-          <div className="mb-3">
-      <h3>Is this a violation</h3>
-      <div className="form-check form-check-inline">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="yes"
-          name="yes"
-          checked={isViolation}
-          onChange={handleCheckboxChange}
-        />
-        <label className="form-check-label" htmlFor="yes">
-          Yes
-        </label>
-      </div>
-      <div className="form-check form-check-inline">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="no"
-          name="no"
-          checked={!isViolation}
-          onChange={handleCheckboxChange}
-        />
-        <label className="form-check-label" htmlFor="no">
-          No
-        </label>
-      </div>
-    </div>
+
+
+          <div className="mb-3 d-sm-flex gap-5">
+          <div className="mb-3 ">
+          <h3>Is this a violation</h3>
+            <div className="form-check form-check-inline">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="yes"
+                name="yes"
+                checked={isViolation}
+                onChange={handleCheckboxChange}
+              />
+              <label className="form-check-label" htmlFor="yes">
+                Yes
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="no"
+                name="no"
+                checked={!isViolation}
+                onChange={handleCheckboxChange}
+              />
+              <label className="form-check-label" htmlFor="no">
+                No
+              </label>
+            </div>
+            </div>
+            <div className="mb-3 d-sm-flex gap-3">
+              <label for="estimate" class="form-label">
+                Estimate
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="estimate"
+                aria-describedby="emailHelp"
+              />
+              <label for="estimate" class="form-label">
+                Estimate
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="estimate"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            </div>
           <button type="submit" class="btn btn-primary">
             Submit
           </button>
