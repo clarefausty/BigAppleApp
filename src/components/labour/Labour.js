7e0c79408d92452223548fde99a1f79d7fcc4da5
clarefausty <clarefausty@gmail.com>
2023-11-25 16:34:57 +0100

    wip

--- src/components/labour/Labour.js
+++ src/components/labour/Labour.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Labour = () => {
   return (
-    <div>Labour</div>
+    <div>
+      <h4>Labour</h4>
+      <div className="mb-3 card-body d-flex gap-5 justify-content-center align-items-center">
+                <label htmlFor="notees" className="form-label w-100">
+                  # of Tees
+                </label>
+                <input
+                  type="number"
+                  className="form-control  py-2"
+                  id="notees"
+                  placeholder="5"
+                />
+              </div>
+              <div className="mb-3 card-body d-flex gap-5 justify-content-center align-items-center">
+                <label htmlFor="notees" className="form-label w-100">
+                  # of Tees
+                </label>
+                <input
+                  type="number"
+                  className="form-control  py-2"
+                  id="notees"
+                  placeholder="5"
+                />
+              </div>
+    </div>
   )
 }
 
